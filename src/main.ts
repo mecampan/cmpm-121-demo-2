30c5dbc7eca02ab4aa36f928a4004715778ea67b
mecampan <mecampan@ucsc.edu>
2024-10-15 11:49:50 -0700

    Step 1 Complete: Added h1 element title, commented out app_name due to duplicate title on screen, and implemented canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import "./style.css";
 
-const APP_NAME = "Notepad++";
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const APP_NAME = "Notepad+++";
+//const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//document.title = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+document.body.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+document.body.appendChild(canvas);
